0f8eaeb36852a005a10285fbea3e7d38d1705446
jackherrmann <56375982+jackherrmann@users.noreply.github.com>
2020-05-03 18:08:19 -0400

    Update bot.py

--- poker192/core/poker/bot.py
+++ poker192/core/poker/bot.py
@@ -17,7 +17,7 @@ def predict(street, board, heroCards):
     hero2 = heroCards[1] 
     hero2s = hero2.suit
     hero2v = hero2.value
-    if len(board) >= 3: 
+    if street >= 1: 
         board1 = board[0] 
         board1s = board1.suit
         board1v = board1.value
@@ -27,11 +27,11 @@ def predict(street, board, heroCards):
         board3 = board[2] 
         board3s = board3.suit
         board3v = board3.value
-    if len(board) >= 4: 
+    if street >= 2: 
         board4 = board[3] 
         board4s = board4.suit
         board4v = board4.value
-    if len(board) >= 5: 
+    if street >= 3: 
         board5 = board[4] 
         board5s = board5.suit
         board5v = board5.value
@@ -55,20 +55,18 @@ def predict(street, board, heroCards):
     list_wrapper = np.array(list_wrapper)
 
     #load the model
-    model_filename = None
+    model = None
 
     if street == 0:
-        model_filename = './preflop_model'
+        model = tf.keras.models.load_model('./saved_models/preflop_model')
     elif street == 1: 
-        model_filename = './postflop_model'
+        model = tf.keras.models.load_model('./saved_models/postflop_model')
     elif street == 2:
-        model_filename = './turn_model'
+        model = tf.keras.models.load_model('./saved_models/turn_model')
     elif street == 3:
-        model_filename = './river_model'
+        model = tf.keras.models.load_model('./saved_models/river_model')
     else:
         return None
-
-    model = tf.keras.models.load_model(model_filename)
 
     #predict
     result = model.predict_classes(list_wrapper)
